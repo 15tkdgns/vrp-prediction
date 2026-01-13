964f2ee8ac1a6f8d17096e71d32126ca75d9c8af
15tkdgns <15tkdgbs@gmail.com>
2026-01-13 20:22:56 +0900

    Cleanup: Remove unused tab_literature.py

--- vrp-prediction/dashboard/tabs/__init__.py
+++ vrp-prediction/dashboard/tabs/__init__.py
@@ -6,7 +6,7 @@ from .tab_methodology import render_methodology
 from .tab_results import render_results
 from .tab_validation import render_validation
 from .tab_references import render_references
-from .tab_literature import render_prior_work_tab
+
 
 __all__ = [
     'render_overview',
@@ -14,5 +14,5 @@ __all__ = [
     'render_results',
     'render_validation',
     'render_references',
-    'render_prior_work_tab'
+
 ]
